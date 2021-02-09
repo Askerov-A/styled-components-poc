a45fc7434da6b6ca16bf2e7932e6deb1360e577e
Azat Askerov <a.askerov@napoleongames.be>
2021-02-09 12:18:14 +0600

    Initial commit with styled components

---
+++ src/components/fields/input/input.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledInput } from "./input.styles";
+
+interface IInputProps {
+
+}
+
+const Input: React.FC<IInputProps> = () => {
+    return <StyledInput />
+}
+
+export default Input;
